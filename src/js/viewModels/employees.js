4d99cd5e339b26ca58d359226109a2053fae6dfb
Ingrid Machado <ingridecomp@gmail.com>
2018-03-21 21:13:34 -0300

    Layout techniques

--- src/js/viewModels/employees.js
+++ src/js/viewModels/employees.js
@@ -7,7 +7,7 @@
 /**
  * employees module
  */
-define(['ojs/ojcore', 'knockout', 'ojs/ojpictochart'
+define(['ojs/ojcore', 'knockout', 'ojs/ojpictochart', 'ojs/ojswitcher', 'ojs/ojfilmstrip', 'ojs/ojknockout'
 ], function (oj, ko) {
     /**
      * The view model for the main content view template
@@ -19,6 +19,26 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojpictochart'
         {name: 'Have Sleep Problems', shape: 'human', count:7, color: '#ed6647'},
         {name: 'Sleep Well', shape: 'human', count: 3}
         ]);
+        
+        self.selectedItem = ko.observable("home");
+        
+        self.chemicals = [
+          { name: 'Hydrogen' },
+          { name: 'Helium' },
+          { name: 'Lithium' },
+          { name: 'Beryllium' },
+          { name: 'Boron' },
+          { name: 'Carbon' },
+          { name: 'Nitrogen' },
+          { name: 'Oxygen' },
+          { name: 'Fluorine' },
+          { name: 'Neon' },
+          { name: 'Sodium' },
+          { name: 'Magnesium' }
+      ];
+      
+      self.currentNavArrowPlacement = ko.observable("adjacent");
+      self.currentNavArrowVisibility = ko.observable("auto");
     }
     
     return employeesContentViewModel;
